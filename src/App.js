faa90c5269dc5874ee2d404411134e71a8e96490
Yuta Home <mizunoyuta1014@gmail.com>
2023-12-08 00:07:51 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getAllPokemon, getPokemon} from './utils/pokemon';
 
 function App() {
+  const initialURL="https://pokeapi.co/api/v2/pokemon";
+  const [loading,setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      // console.log(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  },[])
+
+  const loadPokemon = (data) => {
+    let _pokemonData = Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon)
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+  }
+
   return (
     <div className="App">
+      {loading ? (
+        <h1>ロード中...</h1>
+      ): 
+        <h1>ポケモンデータを取得しました</h1>
+      }
 
     </div>
   );
